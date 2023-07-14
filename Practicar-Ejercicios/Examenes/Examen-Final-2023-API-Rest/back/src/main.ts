94cc2dd8dbfe33841af8b72f3549a9eeb88d19e1
Diego <diegogs2323@gmail.com>
2023-07-14 15:34:51 +0200

    actualizar repo

--- Practicar-Ejercicios/Examenes/Examen-Final-2023-API-Rest/back/src/main.ts
+++ Practicar-Ejercicios/Examenes/Examen-Final-2023-API-Rest/back/src/main.ts
@@ -1,22 +1,16 @@
-
 import { Application, Router } from "oak";
 import { addEvent } from "./resolvers/post.ts";
-import { getEvents ,getEvent} from "./resolvers/get.ts";
+import { getEvents, getEvent } from "./resolvers/get.ts";
 import { deleteEvent } from "./resolvers/delete.ts";
 import { updateEvent } from "./resolvers/put.ts";
 
-
-
 const router = new Router();
 router
-.post("/addEvent", addEvent)
-.get("/events", getEvents)
-.get("/event/:id", getEvent)
-.delete("/deleteEvent/:id",deleteEvent)
-.put("/updateEvent",updateEvent)
-
-
-
+  .post("/addEvent", addEvent)
+  .get("/events", getEvents)
+  .get("/event/:id", getEvent)
+  .delete("/deleteEvent/:id", deleteEvent)
+  .put("/updateEvent", updateEvent);
 
 const app = new Application();
 
